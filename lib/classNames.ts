b9d876428b5271304442fbb532e774ffb071fb8e
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-05-27 02:01:58 -0500

    jsdoc

--- lib/classNames.ts
+++ lib/classNames.ts
@@ -1,19 +1,19 @@
+/**
+ * @example
+ *
+ * import { classNames } from 'lib/classNames';
+ *
+ * <button
+ *   className={classNames(
+ *     'this is always applied',
+ *     isTruthy && 'this only when the isTruthy is truthy',
+ *     active ? 'active classes' : 'inactive classes'
+ * )}>
+ *   Text
+ * </button>
+ */
 export const classNames = (
   ...classes: (false | null | undefined | string)[]
 ): string => {
   return classes.filter(Boolean).join(' ');
 };
-
-// usage example
-//
-// import { classNames } from 'lib/classNames';
-//
-// <button
-//   className={classNames(
-//     'this is always applied',
-//     isTruthy && 'this only when the isTruthy is truthy',
-//     active ? 'active classes' : 'inactive classes'
-//   )}
-// >
-//   Text
-// </button>;
