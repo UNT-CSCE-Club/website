257d0a5d2f71b5446a986adf8cb3b596a3745cf4
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-10-28 22:25:58 -0500

    bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['images.unsplash.com', 'cdn.chec.io'],
   },
   target: 'serverless',
-};
+});
